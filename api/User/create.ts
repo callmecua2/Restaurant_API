2244780c248ddb36a3c10fb828e72ccae784761e
Bob <josua6144@gmail.com>
2026-07-25 21:09:52 +0700

    edit create user endpoint

--- api/User/create.ts
+++ api/User/create.ts
@@ -1,10 +1,25 @@
+import type { Request, Response } from "express";
 import { HashPassword, ComparePassword } from "../../encryption/encryption";
 import prisma from "../../lib/prisma";
+import { UserRole, type Prisma } from "@prisma/client";
 
-export const createUser = async (req: any, res: any) => {
+
+
+interface createUserPayload {
+  username: string;
+  password: string;
+  email?: string;
+  role: UserRole;
+  status?: string;
+}
+
+
+export const createUser = async (
+  req: Request<{}, {}, createUserPayload>,
+  res: Response,
+) => {
   try {
-    const body = await req.json();
-    const { username, email, password, role } = body;
+    const { username, email, password, role } = req.body;
 
     if (!username || !password) {
       return res.status(400).json({
@@ -18,17 +33,6 @@ export const createUser = async (req: any, res: any) => {
     if (password.length < 8) {
       return res.status(400).json({ message: "Password is not long enough" });
     }
-    const existingUser = await prisma.user.findFirst({
-      where: {
-        username: username,
-      },
-    });
-
-    if (!existingUser) {
-      return res.status(400).json({
-        message: "Email has been used",
-      });
-    }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -36,39 +40,73 @@ export const createUser = async (req: any, res: any) => {
       return res.status(400).json({ message: "Invalid email format" });
     }
 
-    const auth = req.user;
-    const userRole = auth.userRole;
-    const organzation = auth.organizationId;
+    const auth = req.user
+    const userRole = auth.userRole
+    const organization = auth.organizationId;
 
-    if (userRole !== "OWNER" || userRole !== "MANAGER") {
-      return res.status(400).json({
-        message: "Not Auhtorized used",
+  
+    //cek jika user role adalah staff akan langsung tolak
+    if (userRole === "STAFF") {
+      return res.status(403).json({
+        message: "Not Authorized User",
       });
     }
 
-    if (role === "MANAGER" && userRole !== "OWNER") {
+
+    //cek jika role ada dikirim tapi tidak ada di enum akan error
+    if (
+      role !== undefined &&
+      !Object.values(UserRole).includes(role)
+    ) {
       return res.status(400).json({
-        message: "Can't add manager account if not by Owner",
+        message: "Invalid role",
+      });
+    }
+
+    // cek jika role kosong atau null nilai default target jadi Staff, jika tidak nilai targetRole adalah role
+    const targetRole = role ?? UserRole.STAFF
+
+    //filter role apa saja yang boleh berdasarkan role user
+    const allowedRoles: Record<UserRole, UserRole[]> = {
+      OWNER: [UserRole.OWNER, UserRole.MANAGER, UserRole.STAFF],
+      MANAGER: [UserRole.MANAGER, UserRole.STAFF],
+      STAFF: [],
+    };
+
+    const allowedRole = allowedRoles[userRole] ?? [];
+
+    //validasi jika role yang dikirim ada atau tidak di role yang boleh diinput
+    if (!allowedRole.includes(targetRole)) {
+      return res.status(403).json({
+        message: "Not Authorized User",
       });
     }
 
     const hashing = await HashPassword(password);
-    const createUser = await prisma.user.create({
-      data: {
-        username: username,
-        password: hashing,
-        role: role,
-        OrganizationId: organzation,
+
+    const userDataToCreate: Prisma.UserCreateInput = {
+      username,
+      password: hashing,
+      email,
+      role : targetRole,
+      userOrganization: {
+        connect: {
+          id: organization,
+        },
       },
+    };
+
+   await prisma.user.create({
+      data: userDataToCreate,
     });
 
-    return res.status(400).json({
+    return res.status(201).json({
       message: "Success creating user",
       user: username,
     });
   } catch (error) {
     console.log(error);
-    return res.status(200).json({
+    return res.status(500).json({
       message: "Internal server error",
     });
   }
