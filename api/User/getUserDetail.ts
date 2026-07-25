9457dc40ad30110d9e685c7e7f342357059d755d
Bob <josua6144@gmail.com>
2026-07-26 00:07:01 +0700

    fix get user detail validation

--- api/User/getUserDetail.ts
+++ api/User/getUserDetail.ts
@@ -1,65 +1,66 @@
-import type { Response } from "express";
+import type { Request, Response } from "express";
 import prisma from "../../lib/prisma";
+import { UserRole } from "@prisma/client";
 
-export const getUserDetail = async (req : any, res: Response) => {
-    try {
-        const userId = Number(req.params.id)
-        
-        if(!userId) {
-            return res.status(400).json({
-                message : "Bad Request"
-            })
-        }
-
-        if(Number.isNaN(userId)) {
-            return res.status(400).json({
-                message : "Bad Request"
-            })
-        }
-
-        const auth = req.user
-        const getUserOrganizationId = auth.organizationId;
-        const getUserOrganizationRole = auth.userRole;
-
-        const allowedRole = ["OWNER", "MANAGER"]
-
-        if(!allowedRole.includes(getUserOrganizationRole)) {
-            return res.status(403).json({
-                message : "Unauthorized User Access"
-            })
-        }
-
-        const findUser = await prisma.user.findFirst({
-            where : {
-                id : userId,
-                OrganizationId : getUserOrganizationId
-            },
-            select : {
-                id : true,
-                username : true,
-                email : true,
-                role : true,
-                createdAt : true,
-                status : true
-            }
-        })
-
-        if(!findUser) {
-            return res.status(404).json({
-                message : "User does not exist"
-            })
-        }
-
-        return res.status(200).json({
-            message : "Success",
-            data : findUser
-        })
-
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            message : "Internal server error"
-        })
-    }
+interface Params {
+  id: string;
 }
+
+export const getUserDetail = async (req: Request<Params>, res: Response) => {
+  try {
+    const userId = Number(req.params.id);
+
+    if (!userId) {
+      return res.status(400).json({
+        message: "Bad Request",
+      });
+    }
+
+    if (Number.isNaN(userId) || userId <= 0) {
+      return res.status(400).json({
+        message: "Bad Request",
+      });
+    }
+
+    const auth = req.user;
+
+    const allowedRole: UserRole[] = [UserRole.OWNER, UserRole.MANAGER];
+
+    if (!allowedRole.includes(auth.userRole)) {
+      return res.status(403).json({
+        message: "Unauthorized User Access",
+      });
+    }
+
+    const findUser = await prisma.user.findFirst({
+      where: {
+        id: userId,
+        OrganizationId: auth.organizationId,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        status: true,
+      },
+    });
+
+    if (!findUser) {
+      return res.status(404).json({
+        message: "User does not exist",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Success",
+      data: findUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
